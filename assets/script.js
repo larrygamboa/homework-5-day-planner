92f0c41289c9455a32852b1d2e8b9506da44ead0
Larry Gamboa <techmedia.lg@gmail.com>
2020-10-29 23:13:05 -0700

    set up date on jumbotron

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
-
-
+    // Set up the date in jumbotron
+    var currentDay = moment().format("[Today is] dddd, MMMM Do YYYY");
+    // Append date to HTML
+    $("#todaysDate").append(currentDay);
+    // Verify the date in console
+    console.log(currentDay);
 });
