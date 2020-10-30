90b991833b2e2bdb4b93b08c5ea754a2b94c1f6f
Larry Gamboa <techmedia.lg@gmail.com>
2020-10-30 11:02:00 -0700

    testing user input

--- assets/script.js
+++ assets/script.js
@@ -15,18 +15,13 @@ $(document).ready(function () {
     // Set up time slot variables
     var inputText9am = $("#inputText9am");
     var inputText10am = $("#inputText10am");
-    // var inputText11am = $("#inputText11am");
-    // var inputText12am = $("#inputText12am");
-    // var inputText1pm = $("#inputText1pm");
-    // var inputText2pm = $("#inputText2pm");
-    // var inputText3pm = $("#inputText3pm");
-    // var inputText4pm = $("#inputText4pm");
-    // var inputText5pm = $("#inputText5pm");
-
-    //
-    // Store the content with local storage for each time slot
-    $("#inputText9am").append(localStorage.getItem("9AM"));
-    $("#inputText10am").append(localStorage.getItem("10AM"));
+    var inputText11am = $("#inputText11am");
+    var inputText12pm = $("#inputText12pm");
+    var inputText1pm = $("#inputText1pm");
+    var inputText2pm = $("#inputText2pm");
+    var inputText3pm = $("#inputText3pm");
+    var inputText4pm = $("#inputText4pm");
+    var inputText5pm = $("#inputText5pm");
 
     //
     // Create event listeners for buttons to set items to local storage
@@ -37,5 +32,15 @@ $(document).ready(function () {
     document.querySelector("#btn10am").addEventListener("click", function () {
         localStorage.setItem("10AM", (inputText10am.val()));
     });
+    //
+    document.querySelector("#btn11am").addEventListener("click", function () {
+        localStorage.setItem("11AM", (inputText11am.val()));
+    });
+
+    //
+    // Store the content with local storage for each time slot
+    $("#inputText9am").append(localStorage.getItem("9AM"));
+    $("#inputText10am").append(localStorage.getItem("10AM"));
+    $("#inputText11am").append(localStorage.getItem("11AM"));
 
 });
